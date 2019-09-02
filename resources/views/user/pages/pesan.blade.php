19a1fcec28539672e5cb9937a773f51ba48f5b69
Adam Ghirvan Fadhil <adam_ghirvan@yahoo.co.id>
2019-09-02 11:11:14 +0700

    ALHAMDULILAH BUG FIXED

--- resources/views/user/pages/pesan.blade.php
+++ resources/views/user/pages/pesan.blade.php
@@ -5,12 +5,18 @@
     <div class="box box-info">
     <div class="box-header">
       <h3  class="box-title">{{ $paket->nama }}</h3>
+      @if (\Session::has('alert-danger'))
+      <div class="alert alert-danger">
+        <a href="{{ route('pesan') }}"><button type="button" class="close" data-dismiss="alert">&times;</button></a>
+        <div>{{Session::get('alert-danger')}}</div>
+      </div>
+      @endif
     </div>
     <!-- /.box-header -->
     <div class="box-body">
             <div class="durasi">{{ $paket->durasi }} hari</div>
             <div class="desc">
-                Paket Start {{ \Carbon\Carbon::parse($paket->starShow)->format('d-m-Y') }}<br>
+                Paket Start {{ \Carbon\Carbon::parse($paket->startShow)->format('d-m-Y') }}<br>
                 Paket End {{ \Carbon\Carbon::parse($paket->endShow)->format('d-m-Y') }}<br>
                 Harga Paket {{ $paket->harga }}
             </div>
@@ -20,29 +26,41 @@
             <input type="hidden" name="harga" value="{{ $paket->harga }}">
             <div class="form-group">
                 <label>Mulai penayangan</label>
-                <div class="input-group">
-                    <div class="input-group-addon">
-                    <i class="fa fa-calendar"></i>
+                <div class="form-group">
+                    <select name="startShow" class="form-control">
+                        <option value="">Choose Date...</option>
+                        @foreach($dates as $keyD => $d)
+                            @if(Carbon\Carbon::parse($d)->format('Y-m-d') > Carbon\Carbon::now()->format('Y-m-d')
+                                ||
+                                Carbon\Carbon::parse($d)->format('Y-m-d') == Carbon\Carbon::now()->format('Y-m-d'))  
+                                <option name="" value="{{ $d }}">{{ Carbon\Carbon::parse($d)->format('d-m-Y') }}</option>
+                            @endif
+                        @endforeach
+                    </select>
+                    @if($errors->has('startShow'))
+                    <div class="text-danger">
+                        {{ $errors->first('startShow')}}
                     </div>
-                    <input type="text" class="form-control pull-right" id="datepicker" name="startShow" autocomplete="off">
-                </div>
-                @if($errors->has('startShow'))
-                <div class="text-danger">
-                    {{ $errors->first('startShow')}}
                 </div>
                 @endif
                 <label>Akhir penayangan</label>
-                <div class="input-group">
-                    <div class="input-group-addon">
-                    <i class="fa fa-calendar"></i>
+                <div class="form-group">
+                    <select name="endShow" class="form-control">
+                        <option value="">Choose Date...</option>
+                        @foreach($dates as $keyD => $d)
+                            @if(Carbon\Carbon::parse($d)->format('Y-m-d') > Carbon\Carbon::now()->format('Y-m-d')
+                                ||
+                                Carbon\Carbon::parse($d)->format('Y-m-d') == Carbon\Carbon::now()->format('Y-m-d'))  
+                                <option name="" value="{{ $d }}">{{ Carbon\Carbon::parse($d)->format('d-m-Y') }}</option>
+                            @endif
+                        @endforeach
+                    </select>
+                    @if($errors->has('endShow'))
+                    <div class="text-danger">
+                        {{ $errors->first('endShow')}}
                     </div>
-                    <input type="text" class="form-control pull-right" id="datepicker2" name="endShow" autocomplete="off">
+                    @endif
                 </div>
-                @if($errors->has('endShow'))
-                <div class="text-danger">
-                    {{ $errors->first('endShow')}}
-                </div>
-                @endif
                 </div>
                 <!-- /.form group -->
                 <button type="submit" class="btn btn-info">Pesan</button>
